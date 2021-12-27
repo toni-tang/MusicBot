2a3fc66cf682a08d677660c52a2c8a6362b21c76
Tony <tonytang213@gmail.com>
2021-12-27 16:13:31 -0600

    Added ability to join and leave voice channels

--- Python/DiscordBot/main.py
+++ Python/DiscordBot/main.py
@@ -1,20 +1,33 @@
 #Harper Bot
 import discord
+from discord.ext import commands
 import os
 
 my_secret = os.environ['token']
-client = discord.Client()
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix = '!', intents = intents)
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event 
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-    
+@client.command(pass_context = True)
+async def join(ctx):
+  if(ctx.author.voice):
+    channel = ctx.message.author.voice.channel
+    await channel.connect()
+  else:
+    await ctx.send("You are not in a voice channel.")
+
+@client.command(pass_context = True)
+async def leave(ctx):
+  if(ctx.voice_client):
+    await ctx.guild.voice_client.disconnect()
+  else:
+    await ctx.send('I am not in a voice channel.')
+
 client.run(my_secret)
 
